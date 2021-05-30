955b82d21a79ed3471dfe452197e1b58ead25399
KrishnaTeja <malakajgirikrishnateja@gmail.com>
2021-05-30 20:03:47 +0530

    updated for loops with new algos

--- for_loops.java
+++ for_loops.java
@@ -1,13 +1,10 @@
 public class for_loops {
-    
-
-//Java Program to demonstrate the example of for loop  
-//which prints table of 1  
-
     public static void main(String[] args) {  
         //Code of Java for loop  
         for(int i=1;i<=10;i++){  
             System.out.println(i);  
-        }  
+        } 
+        System.out.println("for loop completed"); 
+        System.out.println("done for loop"); 
     }  
     } 
