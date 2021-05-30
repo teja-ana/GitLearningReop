96f615da0e912563143859cc101438432f4885d4
KrishnaTeja <malakajgirikrishnateja@gmail.com>
2021-05-30 19:53:46 +0530

    for loops understood

---
+++ for_loops.java
@@ -0,0 +1,13 @@
+public class for_loops {
+    
+
+//Java Program to demonstrate the example of for loop  
+//which prints table of 1  
+
+    public static void main(String[] args) {  
+        //Code of Java for loop  
+        for(int i=1;i<=10;i++){  
+            System.out.println(i);  
+        }  
+    }  
+    } 
